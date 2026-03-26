c3ae8905f69c66f7f7fe634790ae5f3bbf70260f
Ferguson Zibiri <excelzibiri5@gmail.com>
2026-03-26 13:51:55 -0500

    accept css and js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,16 @@
 
 const main = document.querySelector('main');
 const postBtn = document.querySelector('.post-btn');
-const textArea = document.querySelector('.input').value;
+const textArea = document.querySelector('.input');
+const containter = document.querySelector('.container');
+const image = document.querySelector('#imageUpload')
+const now = new Date();
+
+const date = now.toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+});
 
 class User{
     #id;
@@ -66,6 +75,40 @@ class User{
  postBtn.addEventListener('click', (a) => {
     a.preventDefault();
 
+    let textHTML = '';
+    let imgHTML = '';
+
+    if (textArea.value.trim() !== '') {
+        textHTML = `<p class="text-out">${textArea.value}</p>`;
+    }
+
+    if (image.files[0]) {
+        const imageUrl = URL.createObjectURL(image.files[0]);
+        imgHTML = `<img src="${imageUrl}" class="image-out">`;
+    }
+
+    if (!textHTML && !imgHTML) {
+        alert ('Insert content in the box');
+        return;
+    };
+
+
     const post = document.createElement('div');
-    post.textContent = textArea;
+    post.innerHTML = `
+        <div class="top">
+            <div class="end-header">
+                <i class="fa-regular fa-user"></i>
+            </div>          
+            <h3 class="name">Ferguson Zibiri</h3>
+            <p class="date-end">${date}</p>
+        </div>
+        ${textHTML}
+        ${imgHTML}
+    `;
+    post.classList.add('output')
+    main.appendChild(post);
+
+    textArea.value = '';
+    image.value = '';
  })
+
